0783f64c7e5a343796a1be279ced7881139799bd
Unicorn4rich <sk9952908@gmail.com>
2025-04-19 22:37:58 +0500

    drop stash checking

--- main.py
+++ main.py
@@ -2,8 +2,3 @@ print("Helloooooooooooooooo")
 print("Hello")
 print("Hello 1")
 
-print("Hello abc") # ye main branch ki lines hain inhy ham stash karenge dosri branch mein jaen se pehly.
-print("Hello xyz")
-print("Hello 123")
-print("Hello 456")
-
